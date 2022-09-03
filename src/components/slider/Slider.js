5f9d6baa5f21d6e3de565ba7ee700e1bc6a0bb0d
BuiDay <buivanduynhat@gmail.com>
2022-09-04 02:11:59 +0700

    update athur add slider

---
+++ src/components/slider/Slider.js
@@ -0,0 +1,51 @@
+
+import { useEffect, useState } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import {sliderData} from "./Slider-data.js"
+import styles from "./SliderStyle.module.scss"
+
+const Slider = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slideLength = sliderData.length;
+
+    const nextSlide = () => {
+        setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+      };
+    
+      const prevSlide = () => {
+        setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+      };
+
+    useEffect(()=>{
+
+    })
+
+  return (
+    <div className={styles.slider}>
+      <AiOutlineArrowLeft className={`${styles.arrow} ${styles.prev}`} onClick={prevSlide} />
+      <AiOutlineArrowRight className={`${styles.arrow} ${styles.next}`} onClick={nextSlide}/>
+
+      {sliderData.map((slide, index) => {
+        const { image, heading, desc } = slide;
+        return (
+          <div key={index} className={index === currentSlide ? `${styles.slide} ${styles.current}` : `${styles.slide}`} >
+              <>
+                <img src={image} alt="slide" />
+                {index === currentSlide && (
+                <div className={ styles.content}>
+                  <h2>{heading}</h2>
+                  <p>{desc}</p>
+                  <hr />
+                  <a href="#product" className="--btn --btn-primary">
+                    Shop Now
+                  </a>
+                </div>)}
+              </>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Slider;
